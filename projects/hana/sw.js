8b5f978e38613c02b8ed48d0f04c3147187afc3f
Jack <jackmao.x@gmail.com>
2026-08-25 19:51:47 -0400

    Add files via upload

--- projects/hana/sw.js
+++ projects/hana/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hana-v30";
+const CACHE_NAME = "hana-v33";
 const APP_ASSETS = [
   "./",
   "./index.html",
@@ -35,6 +35,19 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
+    );
+    return;
+  }
 
   event.respondWith(
     caches.match(event.request).then((cached) => {
